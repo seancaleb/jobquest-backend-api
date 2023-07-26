d05258c95e4fb198d230b68ca23eedcbf749fd4d
seancaleb <scmubias@gmail.com>
2023-07-26 22:22:46 +0800

    refactor: add http://localhost:4173 to cors origin

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ app.use(loggerMiddleware);
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:4173"],
     credentials: true,
   })
 );
