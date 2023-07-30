11a77c67eb333a5217c0410bff408e3bd7860e70
Sean Caleb <74448818+seancaleb@users.noreply.github.com>
2023-07-30 09:42:54 +0800

    Revert "Development"

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -20,7 +20,6 @@ import { JobIdParams } from "@/schema/job.schema";
 import { AppyJobPostBody, JobApplicationIdParams } from "@/schema/application.schema";
 import Application from "@/models/application.model";
 import { Types } from "mongoose";
-import Session from "@/models/session.model";
 
 /**
  * @desc    Get current user
@@ -95,16 +94,6 @@ const updateUser = async (
     if (!updatedUser) {
       return res.status(404).json({ message: USER_NOT_FOUND });
     }
-
-    // Check if email is present in the session
-    const session = await Session.findOne({ email: user.email }).exec();
-
-    if (session) {
-      await session.deleteOne();
-    }
-
-    // Create a session for the token in the database
-    await Session.create({ email: updatedUser.email });
 
     res.json({ user: updatedUser });
   } catch (error) {
